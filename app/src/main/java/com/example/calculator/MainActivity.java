970904e5969f09e6c0f4d80b47623231f037bdff
HangYongmao <1981217256@qq.com>
2018-02-07 18:57:52 +0800

    实例化按钮

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -2,12 +2,62 @@ package com.example.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btn_0;
+    Button btn_1;
+    Button btn_2;
+    Button btn_3;
+    Button btn_4;
+    Button btn_5;
+    Button btn_6;
+    Button btn_7;
+    Button btn_8;
+    Button btn_9;
+    Button btn_point;
+    Button btn_clear;
+    Button btn_del;
+    Button btn_plus;
+    Button btn_minus;
+    Button btn_multiply;
+    Button btn_divide;
+    Button btn_equal;
+
+    TextView et_input;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btn_0 = (Button) findViewById(R.id.btn_0);
+        btn_1 = (Button) findViewById(R.id.btn_1);
+        btn_2 = (Button) findViewById(R.id.btn_2);
+        btn_3 = (Button) findViewById(R.id.btn_3);
+        btn_4 = (Button) findViewById(R.id.btn_4);
+        btn_5 = (Button) findViewById(R.id.btn_5);
+        btn_6 = (Button) findViewById(R.id.btn_6);
+        btn_7 = (Button) findViewById(R.id.btn_7);
+        btn_8 = (Button) findViewById(R.id.btn_8);
+        btn_9 = (Button) findViewById(R.id.btn_9);
+        btn_point = (Button) findViewById(R.id.btn_point);
+        btn_equal = (Button) findViewById(R.id.btn_equal);
+        btn_del = (Button) findViewById(R.id.btn_del);
+        btn_clear = (Button) findViewById(R.id.btn_clear);
+        btn_minus = (Button) findViewById(R.id.btn_minus);
+        btn_multiply = (Button) findViewById(R.id.btn_multiply);
+        btn_plus = (Button) findViewById(R.id.btn_plus);
+        btn_0 = (Button) findViewById(R.id.btn_divide);
+
+        et_input = (TextView) findViewById(R.id.et_input);
+    }
+
+    @Override
+    public void onClick(View view) {
+
     }
 }
