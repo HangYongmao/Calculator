d111234bd235073a654fee74e8ad484a942daf00
HangYongmao <1981217256@qq.com>
2018-02-07 21:16:37 +0800

    实现业务逻辑

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -26,8 +26,9 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     Button btn_multiply;
     Button btn_divide;
     Button btn_equal;
-
     TextView et_input;
+
+    boolean clear_flag = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,13 +52,149 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         btn_minus = (Button) findViewById(R.id.btn_minus);
         btn_multiply = (Button) findViewById(R.id.btn_multiply);
         btn_plus = (Button) findViewById(R.id.btn_plus);
-        btn_0 = (Button) findViewById(R.id.btn_divide);
-
+        btn_divide = (Button) findViewById(R.id.btn_divide);
         et_input = (TextView) findViewById(R.id.et_input);
+
+        btn_0.setOnClickListener(this);
+        btn_1.setOnClickListener(this);
+        btn_2.setOnClickListener(this);
+        btn_3.setOnClickListener(this);
+        btn_4.setOnClickListener(this);
+        btn_5.setOnClickListener(this);
+        btn_6.setOnClickListener(this);
+        btn_7.setOnClickListener(this);
+        btn_8.setOnClickListener(this);
+        btn_9.setOnClickListener(this);
+        btn_point.setOnClickListener(this);
+        btn_equal.setOnClickListener(this);
+        btn_del.setOnClickListener(this);
+        btn_clear.setOnClickListener(this);
+        btn_minus.setOnClickListener(this);
+        btn_multiply.setOnClickListener(this);
+        btn_plus.setOnClickListener(this);
+        btn_divide.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View view) {
+        String str = et_input.getText().toString();
+        switch (view.getId()) {
+            case R.id.btn_0:
+            case R.id.btn_1:
+            case R.id.btn_2:
+            case R.id.btn_3:
+            case R.id.btn_4:
+            case R.id.btn_5:
+            case R.id.btn_6:
+            case R.id.btn_7:
+            case R.id.btn_8:
+            case R.id.btn_9:
+            case R.id.btn_point:
+                if (clear_flag) {
+                    clear_flag = false;
+                    str = "";
+                    et_input.setText("");
+                }
+                et_input.setText(str + ((Button) view).getText());
+                break;
 
+            case R.id.btn_plus:
+            case R.id.btn_minus:
+            case R.id.btn_multiply:
+            case R.id.btn_divide:
+                if (clear_flag) {
+                    clear_flag = false;
+                    str = "";
+                    et_input.setText("");
+                }
+                et_input.setText(str + " " + ((Button) view).getText() + " ");
+                break;
+
+            case R.id.btn_del:
+                if (clear_flag) {
+                    clear_flag = false;
+                    str = "";
+                    et_input.setText("");
+                } else if (str != null && !str.equals("")) {
+                    et_input.setText(str.substring(0, str.length() - 1));
+                }
+                break;
+
+            case R.id.btn_clear:
+                clear_flag = false;
+                str = "";
+                et_input.setText("");
+                break;
+
+            case R.id.btn_equal:
+                getResult();
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    private void getResult() {
+        String exp = et_input.getText().toString();
+        if (exp == null || exp.equals("")) {
+            return;
+        }
+        if (!exp.contains(" ")) {
+            return;
+        }
+        if (clear_flag == true) {
+            clear_flag = false;
+            return;
+        }
+        clear_flag = true;
+        double result = 0;
+        String s1 = exp.substring(0, exp.indexOf(" "));
+        String op = exp.substring(exp.indexOf(" ") + 1, exp.indexOf(" ") + 2);
+        String s2 = exp.substring(exp.indexOf(" ") + 3);
+        if (!s1.equals("") && !s2.equals("")) {
+            double d1 = Double.parseDouble(s1);
+            double d2 = Double.parseDouble(s2);
+            if (op.equals("＋")) {
+                result = d1 + d2;
+            } else if (op.equals("－")) {
+                result = d1 - d2;
+            } else if (op.equals("×")) {
+                result = d1 * d2;
+            } else if (op.equals("÷")) {
+                if (d2 == 0) {
+                    result = 0;
+                } else {
+                    result = d1 / d2;
+                }
+            }
+            if (!s1.contains(".") && !s2.contains(".") && !op.equals("÷")) {
+                int r = (int) result;
+                et_input.setText(r + "");
+            } else {
+                et_input.setText(result + "");
+            }
+        } else if (!s1.equals("") && s2.equals("")) {
+            et_input.setText(exp);
+        } else if (s1.equals("") && !s2.equals("")) {
+            double d2 = Double.parseDouble(s2);
+            if (op.equals("＋")) {
+                result = 0 + d2;
+            } else if (op.equals("－")) {
+                result = 0 - d2;
+            } else if (op.equals("×")) {
+                result = 0;
+            } else if (op.equals("÷")) {
+                result = 0;
+            }
+            if (!s2.contains(".") && !op.equals("÷")) {
+                int r = (int) result;
+                et_input.setText(r + "");
+            } else {
+                et_input.setText(result + "");
+            }
+        } else if (s1.equals("") && s2.equals("")) {
+            et_input.setText("");
+        }
     }
 }
